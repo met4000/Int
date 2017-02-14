e23f9a7ad1390e3aac9d60f289c4ba2297eb556c
N.R.T <nathsharwor@hotmail.com>
2017-02-14 13:54:49 +0800

    Copy code from gist

---
+++ runme.cmd
@@ -0,0 +1,65 @@
+@echo off
+title How Dare You
+echo Why.
+pause
+echo.
+echo Close this.
+pause
+echo.
+echo No, really.
+pause
+echo.
+echo I'm begging you.
+pause
+echo.
+echo If you don't I'll hack your computer.
+pause
+echo.
+echo You asked for it.
+pause
+echo.
+echo I mean it.
+pause
+echo.
+echo GO AWAY.
+pause
+echo.
+echo ...
+pause
+cls
+tree C:\windows\System32
+pause
+cls
+echo Well, that wasn't as dramatic as I expected.
+pause
+echo.
+echo WHY ARE YOU STILL HERE.
+pause
+echo.
+echo Okay, I know. I'll just turn off this computer.
+pause
+echo.
+echo Then you'll never be able to open this again!
+pause
+echo.
+echo ...
+pause
+echo.
+echo Didn't you hear me?
+pause
+echo.
+echo I'm going to turn this computer off if you don't go away.
+pause
+echo.
+echo REALLY
+pause
+echo.
+echo I MEAN IT
+pause
+echo.
+echo Fine then...
+pause
+echo.
+echo I guess I'll...
+pause
+del runme.cmd & echo @echo off>>runme.cmd & echo title How Dare You>>runme.cmd & echo echo Really. After I even turned off the computer...>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo I know! You'll never be able to annoy me again!>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo Really? You're not even slightly worried? After the power I exercised before?>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo If you keep pestering me I'll format your C drive!>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo ...>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo %userprofile%^^^>del C^^^:^^^\>>runme.cmd & echo echo Access is denied.>>runme.cmd & echo.>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo You have forced me to go to my last resort>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo Really? I will stop you.>>runme.cmd & echo pause>>runme.cmd & echo echo.>>runme.cmd & echo echo Fine then! You leave me no choice...>>runme.cmd & echo pause>>runme.cmd & echo del runme.cmd>>runme.cmd & shutdown /s /t 5 /c "I told you... 5 secs..."
