b0036feed45f99f2fc2ef9f17ac26000443b3054
N.R.T <nathsharwor@hotmail.com>
2017-02-15 18:09:39 +0800

    Clear pause texts

--- runme.cmd
+++ runme.cmd
@@ -3,8 +3,7 @@
 
 @echo off
 title How Dare You - %~nx0
-set message= 
-
+set message=
 call:print Why.
 call:print Close this.
 call:print No, really.
@@ -14,10 +13,10 @@ call:print You asked for it.
 call:print I mean it.
 call:print GO AWAY.
 call:print ...
-cls
+call:clear
 ::tree C:\windows\System32
 pause
-cls
+call:clear
 call:print Well, that wasn't as dramatic as I expected.
 call:print WHY ARE YOU STILL HERE.
 call:print Okay, I know. I'll just turn off this computer.
@@ -30,35 +29,44 @@ call:print I MEAN IT
 call:print Fine then...
 call:print I guess I'll...
 del %~nx0 & (
-  echo @echo off
-  echo title How Dare You - %~nx0
-  echo call:print Really. After I even turned off the computer...
-  echo call:print I know! You can't get out of this!
-  echo call:print Really? You're not even slightly worried? After the power I exercised before?
-  echo call:print If you keep pestering me I'll format your C drive!
-  echo call:print ...
-  echo echo %userprofile%^^^>del C^^^:^^^\
-  echo call:print Access is denied.
-  echo call:print You have forced me to go to my last resort
-  echo call:print Really? I will stop you.
-  echo call:print Fine then! You leave me no choice...
-  echo del %~nx0
-  echo :print
-  echo echo %%*
-  echo set message=%%message%%%%*%$
-  echo pause
-  echo echo.
-  echo goto:EOF
+	echo @echo off
+	echo title How Dare You - %~nx0
+	echo call:print Really. After I even turned off the computer...
+	echo call:print I know! You can't get out of this!
+	echo call:print Really? You're not even slightly worried? After the power I exercised before?
+	echo call:print If you keep pestering me I'll format your C drive!
+	echo call:print ...
+	echo echo %userprofile%^^^>del C^^^:^^^\
+	echo call:print Access is denied.
+	echo call:print You have forced me to go to my last resort
+	echo call:print Really? I will stop you.
+	echo call:print Fine then! You leave me no choice...
+	echo del %~nx0
+	echo :print
+	echo echo %%*
+	echo set message=%%message%%%%*%$
+	echo pause
+	echo echo.
+	echo goto:EOF
 )>>%~nx0
 ::& shutdown /s /t 5 /c "I told you... 5 secs..."
 
 :print
-set message=%message%%*$
-SETLOCAL
-FOR /f "tokens=* delim=$" %%A IN %message% DO (
-	echo %%A
-	echo.
-)
-pause
-ENDLOCAL
+	set message=%message%;%*
+	cls
+	SETLOCAL
+	:nextVar
+		for /F "tokens=1* delims=;" %%a in ("%message%") do (
+			echo %%a
+			echo.
+		set message=%%b
+	)
+	if defined message goto nextVar
+	pause
+	ENDLOCAL
+goto:EOF
+
+:clear
+	set message=
+	cls
 goto:EOF
